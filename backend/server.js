bc038693a4467d36ec81b5a5ac85dc8ec682489d
zlz <1852781063@qq.com>
2025-02-17 14:19:14 +0800

    deploy and build prepare

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 // 加载环境变量到 process.env中
 const dotenv = require('dotenv').config()
@@ -19,6 +20,22 @@ app.use('/api/users', require('./routes/userRoutes'))
 
 // 处理错误
 app.use(errorHandler)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  // 捕获所有未匹配的路由并返回前端页面
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('Please set to production')
+  })
+}
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}`)
